ba4f98ad66ec5fc3c683bbb18aacb59fdc8d6063
Tomtom-dev <54830680+Tomtom-dev@users.noreply.github.com>
2021-09-02 14:37:06 +0200

    add component for film and serie and style it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from 'src/app/app.component'
+import { FilmsComponent } from './films/films.component';
+import { SeriesComponent } from './series/series.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component : AppComponent },
+  {path:"films", component: FilmsComponent},
+  {path:"series", component: SeriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FilmsComponent } from './films/films.component';
+import { SeriesComponent } from './series/series.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FilmsComponent,
+    SeriesComponent
   ],
   imports: [
     BrowserModule,
